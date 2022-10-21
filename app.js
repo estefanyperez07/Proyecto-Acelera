f121bc31d8019d98745504e05a4fd33ea4e64945
eaplicano <89801724+eaplicano@users.noreply.github.com>
2022-10-21 10:47:47 -0600

    Update app.js

--- app.js
+++ app.js
@@ -26,10 +26,6 @@ app
 	.use( morgan('dev') )
 	.use(publicDir)
 	.use(routes)
-	.use(function(req, res, next) {
-  		res.header("Access-Control-Allow-Origin", "localhost")
-  		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  		next()
-})
+	
 
 module.exports = app

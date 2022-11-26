aa6ca1e92d8c1512c88a56fb3a57327ec8d5df02
AndreaLozanoCortes <92347954+AndreaLozanoCortes@users.noreply.github.com>
2022-11-26 17:43:27 -0600

    a

--- models/modulo_seguridad/registro-model.js
+++ models/modulo_seguridad/registro-model.js
@@ -218,7 +218,7 @@ UsuarioModel.autoregistro = async (data, cb) => {
         $2, 
         1,
         $3, 
-        1,
+        5,
         0,
         1,
         (NOW() + interval '${data.paramVigencia} days'), 

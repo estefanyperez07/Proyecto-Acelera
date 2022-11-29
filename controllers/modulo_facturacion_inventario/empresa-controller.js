5fb6769356a2745ee9baeb1f0406a11f1c7c5e9d
eaplicano <eliud.aplicano@unah.hn>
2022-11-28 21:35:28 -0600

    Compras por fecha

---
+++ controllers/modulo_facturacion_inventario/empresa-controller.js
@@ -0,0 +1,127 @@
+"use strict";
+
+var EmpresaModel = require("../../models/modulo_facturacion_inventario/sucursal-model"),
+  EmpresaController = () => {};
+
+EmpresaController.getAll = (req, res, next) => {
+  EmpresaModel.getAll((err, rows) => {
+    if (err) {
+      let locals = {
+        title: "Error al consultar la base de datos",
+        description: "Error de Sintaxis SQL",
+        error: err,
+      };
+
+      res.status(520).json(err);
+    } else {
+      let locals = {
+        title: "Lista de Películas",
+        data: rows,
+      };
+      res.status(200).send(rows.rows);
+      //res.render('index', locals)
+    }
+  });
+};
+
+EmpresaController.getOne = (req, res, next) => {
+  let cod_empresa = req.params.cod_empresa;
+  console.log(cod_empresa);
+
+  EmpresaModel.getOne(cod_empresa, (err, rows) => {
+    console.log(err, "---", rows);
+    if (err) {
+      let locals = {
+        title: `Error al buscar el registro con el id: ${cod_empresa}`,
+        description: "Error de Sintaxis SQL",
+        error: err,
+      };
+
+      res.status(520).json(err);
+    } else {
+      let locals = {
+        title: "Editar Película",
+        data: rows,
+      };
+      res.status(200).send(rows.rows[0]);
+      //res.render('edit-movie', locals)
+    }
+  });
+};
+
+EmpresaController.save = (req, res, next) => {
+  let empresa = {
+    cod_empresa: req.body.cod_sucursal,
+    descripcion: req.body.descripcion,
+    direccion: req.body.direccion,
+    telefono: req.body.telefono,
+    correo: req.body.correo,
+    rtn: req.body.rtn,
+    logo1: req.body.logo1,
+    logo2: req.body.logo2,
+    logo3: req.body.logo3,
+    logo4: req.body.logo4,
+    creado_por: req.body.creado_por,
+    fecha_creacion: req.body.fecha_creacion,
+    modificado_por: req.body.modificado_por,
+    fecha_modificacion: req.body.fecha_modificacion,
+  };
+
+  console.log(empresa);
+
+  EmpresaModel.save(empresa, (err) => {
+    if (err) {
+      let locals = {
+        title: `Error al salvar el registro con el id: ${empresa.cod_empresa}`,
+        description: "Error de Sintaxis SQL",
+        error: err,
+      };
+
+      res.status(520).json(err);
+    } else {
+      res.status(200).send("Success");
+      //res.redirect('/')
+    }
+  });
+};
+
+EmpresaController.delete = (req, res, next) => {
+  let cod_empresa = req.params.cod_sucursal;
+  console.log(cod_empresa);
+
+  EmpresaModel.delete(cod_empresa, (err, rows) => {
+    console.log(err, "---", rows);
+    if (err) {
+      let locals = {
+        title: `Error al eliminar el registro con el id: ${cod_sucursal}`,
+        description: "Error de Sintaxis SQL",
+        error: err,
+      };
+
+      res.status(520).json(err);
+    } else {
+      res.status(200).send("Success");
+      //res.redirect('/')
+    }
+  });
+};
+
+EmpresaController.addForm = (req, res, next) =>
+  res.render("add-movie", { title: "Agregar Película" });
+
+EmpresaController.error404 = (req, res, next) => {
+  let error = new Error(),
+    locals = {
+      title: "Error 404",
+      description: "Recurso No Encontrado",
+      error: error,
+    };
+
+  error.status = 404;
+
+  res.render("error", locals);
+
+  next();
+};
+
+module.exports = EmpresaController;

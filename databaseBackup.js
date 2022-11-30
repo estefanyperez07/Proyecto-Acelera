373106cd441f92a1eb5d9a7b623450f88e7b1ace
eaplicano <eliud.aplicano@unah.hn>
2022-11-29 23:43:36 -0600

    backup

---
+++ databaseBackup.js
@@ -0,0 +1,33 @@
+"use strict";
+// importing required modules
+const { execute } = require("@getvim/execute");
+const dotenv = require("dotenv").config();
+const conf = require("./models/db-conf.json");
+
+// getting db connection parameters from environment file
+const username = conf.user;
+const database = conf.database;
+const dbHost = conf.host;
+const dbPort = conf.port;
+const dbpassword = conf.password;
+
+// defining backup file name
+const date = new Date();
+const today = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+const backupFile = `pg-backup-${today}.tar`;
+
+// writing postgresql backup function
+const takePGBackup = () => {
+  execute(
+    `PGPASSWORD="${dbpassword}" pg_dump -U ${username} -h ${dbHost} -p ${dbPort} -f ${backupFile} -F t -d ${database}`
+  )
+    .then(async () => {
+      console.log(`Backup created successfully`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// calling postgresql backup function
+takePGBackup();

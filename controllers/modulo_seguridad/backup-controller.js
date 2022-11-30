cb2b327d427769f54f8453f3cdbf71bcf4962357
eaplicano <eliud.aplicano@unah.hn>
2022-11-30 00:16:03 -0600

    backup

--- controllers/modulo_seguridad/backup-controller.js
+++ controllers/modulo_seguridad/backup-controller.js
@@ -1,8 +1,7 @@
 "use strict";
 
-const databaseBackup = require("../../backups/databaseBackup");
-
-BackupController = () => {};
+var databaseBackup = require("../../backups/databaseBackup"),
+  BackupController = () => {};
 
 BackupController.save = (req, res, next) => {
   res.satatus(200).json(databaseBackup());
